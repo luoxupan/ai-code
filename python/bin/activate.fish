35ebd07d184153f221d30ff98a6f6467193fce27
luoxupan <luoxupan@didiglobal.com>
2026-03-13 17:36:21 +0800

    update

--- python/bin/activate.fish
+++ python/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/didi/ai-code/python"
+set -gx VIRTUAL_ENV "~/ai-code/python"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
