571cf5858a46659681ce1e19efc1b68c54f56647
luoxupan <luoxupan@xiaomi.com>
2026-05-02 19:11:18 +0800

    update

--- node-server/src/config/index.ts
+++ node-server/src/config/index.ts
@@ -1,18 +1,20 @@
 import { registerAs } from '@nestjs/config';
 
-export default registerAs('config', () => ({
-  app: {
-    port: parseInt(process.env.PORT || '3000', 10),
-    env: process.env.NODE_ENV || 'development',
-  },
-  database: {
-    type: (process.env.DB_TYPE as any) || 'mysql',
-    host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT || '3306', 10),
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD ?? '',
-    database: process.env.DB_DATABASE || 'nest_db',
-    synchronize: process.env.DB_SYNCHRONIZE === 'true',
-    autoLoadEntities: true,
-  },
-}));
+export default registerAs('config', () => {
+  return {
+    app: {
+      port: parseInt(process.env.PORT || '3000', 10),
+      env: process.env.NODE_ENV || 'development',
+    },
+    database: {
+      type: (process.env.DB_TYPE as any) || 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '3306', 10),
+      username: process.env.DB_USERNAME || 'root',
+      password: process.env.DB_PASSWORD ?? '',
+      database: process.env.DB_DATABASE || 'nest_db',
+      synchronize: process.env.DB_SYNCHRONIZE === 'true',
+      autoLoadEntities: true,
+    },
+  };
+});
